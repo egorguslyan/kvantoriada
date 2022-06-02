21a8b1fba8523fc909fcc66b7f1b4a9774cefba9
SimeonGor <simeon.gor@yandex.ru>
2022-06-02 14:39:34 +0300

    Update python serial scripts

--- serial/reader.py
+++ serial/reader.py
@@ -1,14 +1,55 @@
 import matplotlib.pyplot as plt
+import time
+from collections import deque
 
 file = open('dataCalm2.txt')
+s = file.readline()
+
+plt.ion()
+
+s = s[:-1]
+l = s.split()
+y = deque(map(int, l))
+
+plt.clf()
+
+plt.subplot(2, 1, 1)
+plt.plot(y)
+plt.draw()
+#plt.gcf().canvas.flush_events()
+#time.sleep(0.001)
+plt.subplot(2, 1, 2)
+plt.plot(y)
+plt.draw()
+plt.gcf().canvas.flush_events()
+#time.sleep(0.001)
+
+
+size = 16
+
 s = file.readline()
 while s != '':
     s = s[:-1]
     l = s.split()
     data = list(map(int, l))
+    for i in range(0, len(data), size):
+        for j in range(size):
+            y.popleft()
+
+        y.extend(data[i : i + size])
+
+        #plt.ylim(0, 256)
+        plt.clf()
+
+        plt.subplot(2, 1, 1)
+        plt.plot(y)
+        plt.draw()
+        #plt.gcf().canvas.flush_events()
+        #time.sleep(0.001)
+        plt.subplot(2, 1, 2)
+        plt.plot(y)
+        plt.draw()
+        plt.gcf().canvas.flush_events()
+
     s = file.readline()
-    plt.plot(data)
-    plt.show(block=False)
-    plt.pause(0.5)
-    plt.clf()
 file.close()
--- serial/write_read.py
+++ serial/write_read.py
@@ -1,33 +1,59 @@
 import serial
 import matplotlib.pyplot as plt
+from collections import deque
+
+size = 16
+plt.ion()
 
 my_serial = serial.Serial('COM6', 38400, timeout=0.1)
-file = open('dataCalmGSR2.txt', 'a')
+file = open('dataCalmTestGsr.txt', 'a')
+
+y = deque()
+x = deque()
+
 try:
     while True:
         a = int.from_bytes(my_serial.read(), 'little')
         if a == 255:
-            v = [0] * 512
+            v = [0] * size
             i = 0
-            while i < 512:
+            while i < size:
                 if my_serial.in_waiting > 0:
                     v[i] = int.from_bytes(my_serial.read(), 'little')
                     i += 1
-            z = [0] * 512
+            z = [0] * size
             i = 0
-            while i < 512:
+            while i < size:
                 if my_serial.in_waiting > 0:
                     z[i] = int.from_bytes(my_serial.read(), 'little')
                     i += 1
-            plt.close()
-            plt.plot(v, 'b')
-            plt.plot(z, 'r')
-            plt.show(block=False)
+            if len(y) < 512:
+                y.extend(v)
+            else:
+                for j in range(size):
+                    y.popleft()
+                y.extend(v)
+            if len(x) < 512:
+                x.extend(z)
+            else:
+                for j in range(size):
+                    x.popleft()
+                x.extend(z)
+            print(1)
+            plt.clf()
+            print(2)
+            plt.subplot(2, 1, 1)
+            plt.plot(y)
+            plt.draw()
+            plt.subplot(2, 1, 2)
+            plt.plot(x)
+            plt.draw()
+            print(8)
+            plt.gcf().canvas.flush_events()
+            print(9)
             file.write(' '.join(map(str, v)))
-            file.write(',')
-            file.write(' '.join(map(str, z)))
             file.write('\n')
-            plt.pause(0.1)
+            #plt.pause(0.1)
 except KeyboardInterrupt:
     plt.close()
     file.close()
