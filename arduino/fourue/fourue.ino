c864faae780705bdb839f99befb8d2453840c898
SimeonGor <simeon.gor@yandex.ru>
2022-06-02 14:26:02 +0300

    Update arduino scripts

--- arduino/fourue/fourue.ino
+++ arduino/fourue/fourue.ino
@@ -1,4 +1,5 @@
 #define NUM 256
+#define SIZE 16
 #define PINEKG A1
 #define PINGSR A2
 #define GNDEKG 11
@@ -7,14 +8,10 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(8, 7); // RX, TX
-
-byte inputEKG[NUM];
-byte inputGSR[NUM];
 int data;
 
 void setup()
 {
-  Serial.begin(9600);
   mySerial.begin(38400);
   pinMode(GNDEKG, 1);
   pinMode(GNDGSR, 1);
@@ -24,35 +21,25 @@ void setup()
 
 void loop()
 {
-  //uint32_t ti = millis();
-  delay(300);
+  // delay(300);
+  mySerial.write(255);
   for (int i = 0 ; i < NUM; i++) {
     data = analogRead(PINEKG);
-    inputEKG[i] = map(data, 0, 1024, 0, 255);
+    mySerial.write(map(data, 0, 1024, 0, 254));
     delay(5);
   }
+  /*
   digitalWrite(GNDEKG, 0);
   digitalWrite(GNDGSR, 1);
+  mySerial.write(254);
   delay(400);
   for (int i = 0 ; i < 8; i++) {
     data = analogRead(PINGSR);
-    inputGSR[i] = map(data, 0, 1024, 0, 255);
+    for (int j = 0; j < NUM / 8; ++j)
+      mySerial.write(map(data, 0, 1024, 0, 255));
     delay(10);
   }
   digitalWrite(GNDEKG, 1);
   digitalWrite(GNDGSR, 0);
-  //uint16_t rate  = (uint16_t)NUM * 500 / (millis() - ti);
-  //Serial.println(millis()-ti);
-  //uint32_t tim = millis();
-  mySerial.write(255);
-  //mySerial.write(rate);
-  for (int i = 0; i < NUM; i++) {
-    mySerial.write(inputEKG[i]);
-  }
-
-  for (int i = 0; i < NUM; i++) {
-
-    mySerial.write(inputGSR[int(i/32)]);
-  }
-  //Serial.println(millis()-tim);
+  */
 }
