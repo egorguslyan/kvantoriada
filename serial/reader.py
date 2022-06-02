8c864bbefd36f052f6f910d2678ee76f1ec989f5
SimeonGor <simeon.gor@yandex.ru>
2022-06-02 14:41:37 +0300

    Update python serial scripts

--- serial/reader.py
+++ serial/reader.py
@@ -1,46 +1,71 @@
 import matplotlib.pyplot as plt
 from collections import deque
 
-file = open('dataCalmTestGsr2.txt')
-y = deque([128] * 512)
-x = deque([128] * 512)
-size = 16
-plt.ion()
-fig, ax = plt.subplots(2)
-ax[0].set_xlim(0, 512)
-ax[1].set_xlim(0, 512)
-ax[0].set_ylim(0, 255)
-ax[1].set_ylim(0, 255)
-line1, = ax[0].plot(range(512), y)
-line2, = ax[1].plot(range(512), x)
+file1 = open('dataCalmEKG3.txt')
+file2 = open('dataCalmEKG3.txt')
 
-s = file.readline()
+size = 16
+
+y1 = deque([128] * (size*32))
+x1 = deque([128] * (size*32))
+y2 = deque([128] * (size*32))
+x2 = deque([128] * (size*32))
+plt.ion()
+fig, ax = plt.subplots(2, 2)
+for axe in ax.flat:
+    axe.set_xlim(0, (size*32))
+    axe.set_ylim(0, 255)
+line11, = ax[0, 0].plot(range((size*32)), y1)
+line21, = ax[0, 1].plot(range((size*32)), x1)
+line12, = ax[1, 0].plot(range((size*32)), y2)
+line22, = ax[1, 1].plot(range((size*32)), x2)
+
+def unpack_str(sf):
+    sf = sf[:-1]
+    ef, gf = sf.split(',')
+    #ef = sf
+    #gf = ''
+    ekf = ef.split()
+    gsf = gf.split()
+    ekgf = list(map(int, ekf))
+    gsrf = list(map(int, gsf))
+    while len(gsrf) < size:
+        gsrf.append(128)
+    while len(ekgf) < size:
+        ekgf.append(128)
+    return ekgf, gsrf
+
+
+s1 = file1.readline()
+s2 = file2.readline()
 try:
-    while s != '':
-        s = s[:-1]
-        e, g = s.split(',')
-        ek = e.split()
-        gs = g.split()
-        ekg = list(map(int, ek))
-        gsr = list(map(int, gs))
-        while len(gsr) < size:
-            gsr.append(128)
-        while len(ekg) < size:
-            ekg.append(128)
+    while s1 != '' and s2 != '':
+        ekg1, gsr1 = unpack_str(s1)
+        ekg2, gsr2 = unpack_str(s2)
         for i in range(size):
-            y.popleft()
-            x.popleft()
-        y.extend(ekg)
-        x.extend(gsr)
-        line1.set_ydata(y)
-        line2.set_ydata(x)
+            y1.popleft()
+            x1.popleft()
+            y2.popleft()
+            x2.popleft()
+        y1.extend(ekg1)
+        x1.extend(gsr1)
+        y2.extend(ekg2)
+        x2.extend(gsr2)
+        line11.set_ydata(y1)
+        line21.set_ydata(x1)
+        line12.set_ydata(y2)
+        line22.set_ydata(x2)
         fig.canvas.draw()
         fig.canvas.flush_events()
-        s = file.readline()
+        s1 = file1.readline()
+        s2 = file2.readline()
+
 except KeyboardInterrupt:
     plt.close()
-    file.close()
+    file1.close()
+    file2.close()
     print('Bye')
 finally:
-    file.close()
+    file1.close()
+    file2.close()
     plt.close()
--- serial/write_read.py
+++ serial/write_read.py
@@ -1,15 +1,18 @@
 import serial
 import matplotlib.pyplot as plt
 from collections import deque
+import time
 
-size = 256
-plt.ion()
+size = 32
+size_package = 256
 
 my_serial = serial.Serial('COM6', 38400, timeout=0.1)
-file = open('dataCalmEKG28_2.txt', 'a')
+file = open('dataLightStressEKG_30_4.txt', 'a')
 
 y = deque([128]*512)
 x = deque([128]*512)
+
+plt.ion()
 fig, ax = plt.subplots(2)
 ax[0].set_xlim(0, 512)
 ax[1].set_xlim(0, 512)
@@ -19,42 +22,59 @@ line1, = ax[0].plot(range(512), y)
 line2, = ax[1].plot(range(512), x)
 
 try:
-    while True:
+    start = time.time()
+    while time.time() - start < 20:
         a = int.from_bytes(my_serial.read(), 'little')
         if a == 255:
-            v = [0] * size
-            i = 0
-            while i < size:
-                if my_serial.in_waiting > 0:
-                    v[i] = int.from_bytes(my_serial.read(), 'little')
-                    i += 1
-            z = [0] * size
-            i = 0
-            while i < size:
-                if my_serial.in_waiting > 0:
-                    z[i] = int.from_bytes(my_serial.read(), 'little')
-                    i += 1
-            if len(y) < 512:
-                y.extend(v)
-            else:
-                for j in range(size):
-                    y.popleft()
-                y.extend(v)
-            if len(x) < 512:
-                x.extend(z)
-            else:
-                for j in range(size):
-                    x.popleft()
-                x.extend(z)
-            line1.set_ydata(y)
-            line2.set_ydata(x)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-            file.write(' '.join(map(str, v)))
-            file.write(',')
-            file.write(' '.join(map(str, z)))
+            for _ in range(size_package // size):
+                v = [0] * size
+                i = 0
+                while i < size:
+                    if my_serial.in_waiting > 0:
+                        v[i] = int.from_bytes(my_serial.read(), 'little')
+                        i += 1
+
+                if len(y) < 512:
+                    y.extend(v)
+                else:
+                    for j in range(size):
+                        y.popleft()
+                    y.extend(v)
+
+                line1.set_ydata(y)
+
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+                file.write(' '.join(map(str, v)))
+                file.write(' ')
+        elif a == 254:
+            for _ in range(size_package // size):
+                v = [0] * size
+                i = 0
+                while i < size:
+                    if my_serial.in_waiting > 0:
+                        v[i] = int.from_bytes(my_serial.read(), 'little')
+                        i += 1
+
+                if len(x) < 512:
+                    x.extend(v)
+                else:
+                    for j in range(size):
+                        x.popleft()
+                    x.extend(v)
+
+                line2.set_ydata(x)
+
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+                file.write(' '.join(map(str, v)))
+                file.write(' ')
+
             file.write('\n')
             #plt.pause(0.1)
+    plt.close()
+    file.close()
+    my_serial.close()
 except KeyboardInterrupt:
     plt.close()
     file.close()
