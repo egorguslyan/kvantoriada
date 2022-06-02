e80c8ef0e664b8faadece89307bf493e7ddcf32e
SimeonGor <simeon.gor@yandex.ru>
2022-06-02 14:28:58 +0300

    Update arduino scripts

--- arduino/write/write.ino
+++ arduino/write/write.ino
@@ -2,6 +2,7 @@
 #define NUM 200L * TIME
 #define SIZE 16
 
+#define PINEEG A2
 #define PINEKG A1
 #define PINGSR A2
 #define GNDEKG 11
@@ -71,18 +72,16 @@ void loop()
     mySerial.write(map(data, 0, 1024, 0, 254));
     delay(5);
   }
-  /*
-  digitalWrite(GNDEKG, 0);
-  digitalWrite(GNDGSR, 1);
-  mySerial.write(254);
-  delay(400);
-  for (int i = 0 ; i < 8; i++) {
-    data = analogRead(PINGSR);
-    for (int j = 0; j < NUM / 8; ++j)
-      mySerial.write(map(data, 0, 1024, 0, 255));
-    delay(10);
+  digitalWrite(PINLED, LOW);
+  while (digitalRead(PINBTN) == LOW) {
+    data = analogRead(PINEKG);
+    plotter(1023 - data);
   }
-  digitalWrite(GNDEKG, 1);
-  digitalWrite(GNDGSR, 0);
-  */
+  digitalWrite(PINLED, HIGH);
+  mySerial.write(254);
+  for (long i = 0 ; i < NUM; i++) {
+    data = analogRead(PINEEG);
+    mySerial.write(map(data, 0, 1024, 0, 254));
+    delay(5);
+  }
 }
