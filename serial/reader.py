7532f68bec6cc9af3a09f19329a49e9e59e61ed9
SimeonGor <simeon.gor@yandex.ru>
2022-06-02 14:38:35 +0300

    Update python serial scripts

--- serial/reader.py
+++ serial/reader.py
@@ -1,24 +1,14 @@
 import matplotlib.pyplot as plt
-import time
 
-def f(o):
-    o = int(o)
-    if o > 255:
-        return 128
-    else:
-        return o
-
-
-file = open('data1.txt')
+file = open('dataCalm2.txt')
 s = file.readline()
-d = list(map(f, s.split()))
-print(len(d))
-for i in range(56):
-    e = d[i*512:(i+1)*512-1]
-    e.append(128)
-    #print(e)
-    plt.plot(e)
+while s != '':
+    s = s[:-1]
+    l = s.split()
+    data = list(map(int, l))
+    s = file.readline()
+    plt.plot(data)
     plt.show(block=False)
     plt.pause(0.5)
-    plt.close()
+    plt.clf()
 file.close()
