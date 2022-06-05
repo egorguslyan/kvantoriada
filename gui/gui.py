3248a8b8eb1c385a169255dd8dc377541c3290e1
SiemonGor <simeon.gor@yandex.ru>
2022-06-05 16:51:13 +0300

    Update GUI script (add age count)

--- gui/gui.py
+++ gui/gui.py
@@ -27,7 +27,6 @@ class Window(QtWidgets.QMainWindow):
         self.ui.ageNumberLable.setText(str(age))
 
 
-
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     application = Window()
