c23743f2643b4b82d98e6a449ffc6d8dac33df64
Egor Guslyancev <egorguslyan@gmail.com>
2022-06-14 12:17:33 +0300

    Measure event by pressing button

--- arduino/sandbox/sandbox.ino
+++ arduino/sandbox/sandbox.ino
@@ -1,6 +1,7 @@
 #define ARR_SIZE 256
-#define GSR_TIME 300
-#define ECG_TIME 1000
+#define GSR_TIME 10000
+#define ECG_TIME 15000
+#define BTN 5
 
 struct module
 {
@@ -52,25 +53,40 @@ struct
         state = !state;
         updpins();
     }
+    void disable()
+    {
+        digitalWrite(pin, LOW);
+        digitalWrite(not_pin, LOW);
+    }
 } control;
 
 module GSR;
+module ECG;
+module EEG;
+
+uint8_t intr_counter;
+uint8_t start;
 
 void setup()
 {
+    pinMode(BTN, INPUT);
     pinMode(13, OUTPUT);
     control.pin = A3;
     control.not_pin = 12;
-    control.state = 1;
     control.setup();
+    control.disable();
     GSR.pin = A2;
     GSR.setup();
+    ECG.pin = A1;
+    ECG.setup();
+    EEG.pin = A0;
+    EEG.setup();
     noInterrupts(); {
         TCCR1A = 0;
         TCCR1B = 0;
         TIMSK1 = (1 << TOIE1);
         TCCR1B |= (1 << CS10);
-        //TCCR1B |= (1 << CS11);
+        // TCCR1B |= (1 << CS11);
     } interrupts();
     Serial.begin(9600);
     delay(10);
@@ -80,8 +96,8 @@ void loop()
 {
     char S[10];
     static uint32_t timer0, timer1;
-    volatile uint8_t gsr[ARR_SIZE];
-    uint8_t i;
+    volatile uint8_t gsr[ARR_SIZE], ecg[ARR_SIZE], eeg[ARR_SIZE];
+    uint8_t i, btn, prevBtn;
 
     if(millis() - timer0 > 100)
     {
@@ -89,27 +105,58 @@ void loop()
         delay(50);
     } delay(5);
 
-    if(millis() - timer1 > (control.state ? GSR_TIME : ECG_TIME))
+    if((millis() - timer1) > (control.state ? GSR_TIME : ECG_TIME))
     {
         timer1 = millis();
-        control.toggle();
+        if(start) control.toggle();
+        if(control.state) 
+        {
+            start = 0;
+            control.disable();
+        }
     }
 
-    // if(control.state)
-        GSR.pop(gsr);
-    shiftOut(11, 11, 0, gsr[0]);    // volatile didn't help
-    for(i = 1; i <= gsr[0]; i++)
+    btn = digitalRead(BTN);
+    if(!prevBtn && btn)
     {
-        sprintf(S, "%d %d", 0, control.state ? gsr[i] : 0);
-        Serial.println(S);
+        start = 1;
+        timer1 = millis();
+        control.set(1);
     }
-    gsr[0] = 0;
+    prevBtn = btn;
+
+    GSR.pop(gsr);
+    ECG.pop(ecg);
+    EEG.pop(eeg);
+    shiftOut(11, 11, 0, gsr[0]);    // volatile didn't help
+    if(control.state)
+        for(i = 1; i <= gsr[0]; i++)
+        {
+            sprintf(S, "%d %d %d", 0, 0, gsr[i]);
+            Serial.println(S);
+        }
+    else
+        for(i = 1; i <= ecg[0]; i++)
+        {
+            sprintf(S, "%d %d %d", ecg[i], eeg[i], 0);
+            Serial.println(S);
+        }
+    gsr[0] = ecg[0] = eeg[0] = 0;
 }
 
 ISR(TIMER1_OVF_vect)
 {
-    digitalWrite(13, HIGH);
-    GSR.enabled = control.state;
-    GSR.read();
-    digitalWrite(13, LOW);
+    intr_counter++;
+    if((intr_counter >= 10) && start)
+    {
+        intr_counter = 0;
+        digitalWrite(13, HIGH);
+        GSR.enabled = control.state;
+        ECG.enabled = !control.state;
+        EEG.enabled = !control.state;
+        GSR.read();
+        ECG.read();
+        EEG.read();
+        digitalWrite(13, LOW);
+    }
 }
