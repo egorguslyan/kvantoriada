6e47c1db1b9268c0e6bafa5a2bca0076dee39f29
Egor Guslyancev <egorguslyan@gmail.com>
2022-06-28 11:27:35 +0300

    Убрал прерывания и буфер, починил кнопку

--- arduino/sandbox/sandbox.ino
+++ arduino/sandbox/sandbox.ino
@@ -1,3 +1,11 @@
+#define DEBUG_LED 13
+#define BTN 5
+
+/* После изменения стандартных значений времени
+   нужно обязательно запустить gen.bat или gen.sh */
+#define GSR_TIME 5
+#define ECG_TIME 20
+
 #include <EEPROM.h>
 /*    Mem map    **
     0) GSR1
@@ -12,32 +20,20 @@
     #error "Сгенерируйте ключ с помощью gen.bat или gen.sh"
 #endif
 
-#define ARR_SIZE 256
-#define GSR_TIME 5
-#define ECG_TIME 20
-#define BTN 5
+#define GSR_T ((uint32_t)control.seconds0 * 1000)
+#define ECG_T ((uint32_t)control.seconds1 * 1000)
 
 struct module
 {
     uint8_t pin : 5;
     uint8_t enabled : 1;
-    volatile uint8_t buffer[ARR_SIZE];
     void setup()
     {
         pinMode(pin, INPUT);
     }
-    void read()
+    uint8_t read()
     {
-        if((buffer[0] != ARR_SIZE - 1) && enabled)
-        {
-            buffer[0]++;
-            buffer[buffer[0]] = analogRead(pin);
-        }
-    }
-    void pop(uint8_t *target)
-    {
-        memcpy(target, buffer, sizeof(uint8_t) * ARR_SIZE);
-        buffer[0] = 0;
+        return enabled ? analogRead(pin) : 0;
     }
 };
 
@@ -47,7 +43,7 @@ struct
     uint16_t seconds1 : 9;
     uint8_t pin : 5;
     uint8_t not_pin : 5;
-    volatile uint8_t state : 1;
+    uint8_t state : 1;
     uint8_t enabled : 1;
     void updpins()
     {
@@ -82,6 +78,14 @@ struct
     }
 } control;
 
+enum
+{
+    SER_GSR = 'G',
+    SER_ECG = 'C',
+    SER_EEG = 'E',
+};
+const char SERKEYS[3] = {SER_GSR, SER_ECG, SER_EEG};
+
 module GSR;
 module ECG;
 module EEG;
@@ -114,6 +118,8 @@ uint8_t eeprom_byte_form(uint8_t b, uint8_t useful_bits)
     return r;
 }
 
+extern volatile unsigned long timer0_millis;
+
 void setup()
 {
     uint8_t eepromData, i;
@@ -129,6 +135,7 @@ void setup()
     ECG.setup();
     EEG.pin = A0;
     EEG.setup();
+    control.seconds0 = control.seconds1 = 0;
     for(i = 0; i < 4; i++)
     {
         eepromData = EEPROM.read(i);
@@ -155,33 +162,29 @@ void setup()
         }
         switch(i)
         {
-            case 0: {
+            case 1: {
                 control.seconds0 &= 0b111100000;
                 control.seconds0 |= eepromData & 0b11111;
             } break;
-            case 2: {
+            case 3: {
                 control.seconds1 &= 0b111100000;
                 control.seconds1 |= eepromData & 0b11111;
             } break;
-            case 1: {
+            case 0: {
                 control.seconds0 &= 0b000011111;
                 control.seconds0 |= (eepromData & 0b1111) << 5;
                 GSR.enabled = (eepromData & 0b10000) >> 4;
             } break;
-            case 3: {
+            case 2: {
                 control.seconds1 &= 0b000011111;
                 control.seconds1 |= (eepromData & 0b1111) << 5;
                 ECG.enabled = EEG.enabled = (eepromData & 0b10000) >> 4;
             } break;
         }
     }
-    noInterrupts(); {
-        TCCR1A = 0;
-        TCCR1B = 0;
-        TIMSK1 = (1 << TOIE1);
-        TCCR1B |= (1 << CS10);
-        // TCCR1B |= (1 << CS11);
-    } interrupts();
+    noInterrupts();
+    timer0_millis = GSR_T + ECG_T;
+    interrupts();
     Serial.begin(9600);
     delay(10);
 }
@@ -189,71 +192,59 @@ void setup()
 void loop()
 {
     char S[10];
-    static uint32_t timer0, timer1, timer2;
-    volatile uint8_t gsr[ARR_SIZE], ecg[ARR_SIZE], eeg[ARR_SIZE];
+    static uint32_t timer0 = 0, timer1 = 0, timer2 = 0, timer3 = 0;
+    uint8_t gsr, ecg, eeg;
+    const uint8_t *mod[3] = {&gsr, &ecg, &eeg};
     uint8_t i, btn, prevBtn;
 
     // Нагрузка
-    if(millis() - timer0 > 100)
-    {
-        timer0 = millis();
-        delay(50);
-    } delay(5);
+    // if(millis() - timer0 > 100)
+    // {
+    //     timer0 = millis();
+    //     delay(50);
+    // } delay(5);
 
-    if(millis() - timer1 > ((uint32_t)(control.state ? control.seconds0 : control.seconds1) * 1000))
+    if(millis() - timer1 > (control.state ? GSR_T : ECG_T))
     {
         timer1 = millis();
         control.toggle();
     }
 
-    control.enabled = GSR.enabled = ECG.enabled = EEG.enabled
-        = !(millis() - timer2 > ((uint32_t)(control.seconds0 + control.seconds1) * 1000));
+    control.enabled = !(millis() - timer2 > (GSR_T + ECG_T));
     if(!control.enabled)
+    {
         control.disable();
+        GSR.enabled =
+        ECG.enabled =
+        EEG.enabled = 0;
+    }
+    else
+    {
+        GSR.enabled = control.state;
+        ECG.enabled = !control.state;
+        EEG.enabled = !control.state;
+    }
 
     btn = digitalRead(BTN);
     if(btn && !prevBtn)
         timer1 = timer2 = millis();
     prevBtn = btn;
 
-    GSR.pop(gsr);
-    ECG.pop(ecg);
-    EEG.pop(eeg);
-    shiftOut(11, 11, 0, gsr[0]);    // volatile didn't help
-    shiftOut(11, 11, 0, ecg[0]);
-    shiftOut(11, 11, 0, eeg[0]);
-    if(control.state)
-        for(i = 1; i <= gsr[0]; i++)
-        {
-            sprintf(S, "%d %d %d", 0, 0, gsr[i]);
-            Serial.println(S);
-        }
-    else
-        for(i = 1; i <= ecg[0]; i++)
-        {
-            sprintf(S, "%d %d %d", ecg[i], eeg[i], 0);
-            Serial.println(S);
-        }
-    gsr[0] = ecg[0] = eeg[0] = 0;
-}
-
-ISR(TIMER1_OVF_vect)
-{
-    intr_counter++;
-    if(intr_counter >= 10)
+    if(millis() - timer3 > 1)
     {
-        intr_counter = 0;
+        timer3 = millis();
         digitalWrite(13, HIGH);
-        if(control.enabled)
-        {
-            if(control.state)
-                GSR.read();
-            else
-            {
-                ECG.read();
-                EEG.read();
-            }
-        }
+        gsr = GSR.read();
+        ecg = ECG.read();
+        eeg = EEG.read();
         digitalWrite(13, LOW);
     }
+
+    for(i = 0; i < 3; i++)
+        if(*mod[i] != 0)
+        {
+            Serial.print(SERKEYS[i]);
+            Serial.print(*mod[i]);
+            Serial.print(';');
+        }
 }
