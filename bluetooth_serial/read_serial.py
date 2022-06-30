212a9bd3e584531e487f304999f04294de225d7c
SimeonGor <simeon.gor@yandex.ru>
2022-07-01 00:54:58 +0300

    read_serial.py add exception

--- bluetooth_serial/read_serial.py
+++ bluetooth_serial/read_serial.py
@@ -1,6 +1,5 @@
 import serial
 import serial.tools.list_ports
-import time
 import pandas as pd
 
 
@@ -19,62 +18,65 @@ def read(com, file_path, timeECG=15, timeEEG=15, timeGSR=5, enableECG=1, enableE
     if com not in available_ports:
         return False
 
-    with serial.Serial(com, 38400, timeout=0.1) as my_serial, open('log.txt', 'w') as log:
-        ecg, eeg, gsr = [], [], []
-        while my_serial.in_waiting <= 0:
-            pass
-        a = chr(int.from_bytes(my_serial.read(), "little"))
-        log.write(a)
-        while a != 's':
-            if my_serial.in_waiting > 0:
-                a = chr(int.from_bytes(my_serial.read(), "little"))
-                log.write(a)
-                print('1', a)
-        my_serial.write(str_cfg.encode('ascii'))
-        while True:
+    try:
+        with serial.Serial(com, 38400, timeout=0.1) as my_serial, open('log.txt', 'w') as log:
+            ecg, eeg, gsr = [], [], []
             while my_serial.in_waiting <= 0:
                 pass
             a = chr(int.from_bytes(my_serial.read(), "little"))
             log.write(a)
-            while a != 'G' and a != 'C' and a != 'E' and a != 'f':
+            while a != 's':
                 if my_serial.in_waiting > 0:
                     a = chr(int.from_bytes(my_serial.read(), "little"))
-                    print('2', a)
                     log.write(a)
-
-            if a == 'f':
-                log.write(a)
-                break
-
-            v = ' '
-            while v != ';':
-                i = 0
+                    print('1', a)
+            my_serial.write(str_cfg.encode('ascii'))
+            while True:
                 while my_serial.in_waiting <= 0:
                     pass
-                v = chr(int.from_bytes(my_serial.read(), "little"))
-                log.write(v)
-                while v != ',' and v != ';':
+                a = chr(int.from_bytes(my_serial.read(), "little"))
+                log.write(a)
+                while a != 'G' and a != 'C' and a != 'E' and a != 'f':
                     if my_serial.in_waiting > 0:
-                        i *= 10
-                        i += int(v)
-                        v = chr(int.from_bytes(my_serial.read(), "little"))
-                        log.write(v)
-                log.write('(' + str(i) + ')')
-                if a == 'G':
-                    gsr.append(i)
-                elif a == 'C':
-                    ecg.append(i)
-                elif a == 'E':
-                    eeg.append(i)
+                        a = chr(int.from_bytes(my_serial.read(), "little"))
+                        print('2', a)
+                        log.write(a)
 
-        gsr = [0, 0]
-        ecg = ' '.join(map(str, ecg))
-        eeg = ' '.join(map(str, eeg))
-        gsr = ' '.join(map(str, gsr))
-        data = pd.DataFrame([[ecg, eeg, gsr]], columns=['ecg', 'eeg', 'gsr'])
-        data.to_csv(file_path, index=False)
+                if a == 'f':
+                    log.write(a)
+                    break
 
-    return True
+                v = ' '
+                while v != ';':
+                    i = 0
+                    while my_serial.in_waiting <= 0:
+                        pass
+                    v = chr(int.from_bytes(my_serial.read(), "little"))
+                    log.write(v)
+                    while v != ',' and v != ';':
+                        if my_serial.in_waiting > 0:
+                            i *= 10
+                            i += int(v)
+                            v = chr(int.from_bytes(my_serial.read(), "little"))
+                            log.write(v)
+                    log.write('(' + str(i) + ')')
+                    if a == 'G':
+                        gsr.append(i)
+                    elif a == 'C':
+                        ecg.append(i)
+                    elif a == 'E':
+                        eeg.append(i)
+
+            gsr = [0, 0]
+            ecg = ' '.join(map(str, ecg))
+            eeg = ' '.join(map(str, eeg))
+            gsr = ' '.join(map(str, gsr))
+            data = pd.DataFrame([[ecg, eeg, gsr]], columns=['ecg', 'eeg', 'gsr'])
+            data.to_csv(file_path, index=False)
+
+        return True
+    except serial.serialutil.SerialException:
+        return False
 
 
 if __name__ == "__main__":
