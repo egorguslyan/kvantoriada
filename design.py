ad5d88439a9967850881398f05312f72b939c9e3
SimeonGor <simeon.gor@yandex.ru>
2022-10-14 20:48:31 +0300

    120 sec

--- design.py
+++ design.py
@@ -720,7 +720,7 @@ class Ui_MainWindow(object):
         self.timeECG.setStyleSheet("background-color: rgb(255, 255, 255);\n"
 "")
         self.timeECG.setMinimum(10)
-        self.timeECG.setMaximum(180)
+        self.timeECG.setMaximum(120)
         self.timeECG.setObjectName("timeECG")
         self.horizontalLayout_5.addWidget(self.timeECG)
         self.label_10 = QtWidgets.QLabel(self.settings)
@@ -806,7 +806,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
-        self.tab.setCurrentIndex(0)
+        self.tab.setCurrentIndex(2)
         self.tabWidget_3.setCurrentIndex(0)
         self.tabWidget_2.setCurrentIndex(0)
         self.tabWidget.setCurrentIndex(0)
@@ -844,8 +844,8 @@ class Ui_MainWindow(object):
         self.recommendationsText.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8.25pt;\">Рекомендации</span></p></body></html>"))
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Рекомендации</p></body></html>"))
         self.editRecommendationsButton.setText(_translate("MainWindow", "Изменить"))
         self.tab.setTabText(self.tab.indexOf(self.card), _translate("MainWindow", "Карточка"))
         self.label_20.setText(_translate("MainWindow", "Выберите файл"))
